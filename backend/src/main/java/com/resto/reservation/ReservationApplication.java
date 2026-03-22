798185599e402b27daa0c44b765c548e2494387b
geilu <geilyntisler1@gmail.com>
2026-03-22 23:38:21 +0200

    remove useless import

--- backend/src/main/java/com/resto/reservation/ReservationApplication.java
+++ backend/src/main/java/com/resto/reservation/ReservationApplication.java
@@ -2,7 +2,6 @@ package com.resto.reservation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
 
 import java.util.TimeZone;
 
